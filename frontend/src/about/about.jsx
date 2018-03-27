868f6f3b7fe3b694c49163718e22161062b277a6
João Harger <joao-harger@hotmail.com>
2018-03-27 08:32:47 -0300

    Initial commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>(
+  <div>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+    <h2> Nossa Historia</h2>
+    <p>Lalalalallala</p>
+    <p>Lalalalallala</p>
+    <p>Lalalalallala</p>
+    <p>Lalalalallala</p>
+    <h2>Legal!</h2>
+  </div>
+)
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+  <div>
+    <h1>Aqui vai a lista</h1>
+  </div>
+)
